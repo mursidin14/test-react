1c03270f59cd320305672dc194048cac3d93208d
mursidin14 <mursidinrm14@gmail.com>
2020-06-01 08:53:04 +0800

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -3,6 +3,5 @@ export function handler(event, context, callback) {
   console.log('queryStringParameters', event.queryStringParameters)
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, bang!' }),
   })
 }
